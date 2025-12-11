5332f709c54bfa04785b18c7bfb30965c9f20835
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2025-12-11 11:41:08 +0100

    feat: update build scripts to fetch Modrinth data before dev and build
    refactor(PluginsList): simplify button rendering and remove unused elements
    
    chore: auto-generate plugins data from Modrinth API
    
    fix: update modpack info and plugin details for new version

--- src/data/plugins.js
+++ src/data/plugins.js
@@ -1,62 +1,364 @@
-// Plugin/Mod data for Hak SMP Season 4
+// Auto-generated from Modrinth API on 2025-12-11
+// Modpack ID: A1fB3OWk
+// DO NOT EDIT MANUALLY - changes will be overwritten on next build
+
 export const plugins = [
   {
-    id: 1,
-    name: "Plugin Name 1",
-    description: "description",
-    version: "1.0.0",
-    required: true,
-    downloadUrl: "#",
-    author: "Plugin Author 1",
-    inNormalPack: true,
-    inPerformancePack: true,
+    "id": "4I1XuqiY",
+    "name": "[EMF] Entity Model Features",
+    "description": "EMF is an, OptiFine format, Custom Entity Model replacement mod available for Fabric and Forge.",
+    "version": "3.0.1",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/4I1XuqiY/versions/PHCCbdMs/entity_model_features_1.21.6-fabric-3.0.1.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
   },
   {
-    id: 2,
-    name: "Plugin Name 2",
-    description: "description",
-    version: "2.5.3",
-    required: false,
-    downloadUrl: "#",
-    author: "Plugin Author 2",
-    inNormalPack: true,
-    inPerformancePack: false,
+    "id": "MPCX6s5C",
+    "name": "Not Enough Animations",
+    "description": "Bringing first-person animations to the third-person",
+    "version": "1.10.6",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/MPCX6s5C/versions/RB2sBFu8/notenoughanimations-fabric-1.10.6-mc1.21.8.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
   },
   {
-    id: 3,
-    name: "Plugin Name 3",
-    description: "description",
-    version: "3.0.0",
-    required: true,
-    downloadUrl: "#",
-    author: "Plugin Author 3",
-    inNormalPack: true,
-    inPerformancePack: true,
+    "id": "Wb5oqrBJ",
+    "name": "Chat Heads",
+    "description": "See who you're chatting with!",
+    "version": "0.14.0",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/Wb5oqrBJ/versions/P6q2wonI/chat_heads-0.14.0-fabric-1.21.6.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
   },
+  {
+    "id": "50dA9Sha",
+    "name": "Fresh Animations",
+    "description": "Make your game like the trailers! Dynamic animated entities to freshen your Minecraft experience.",
+    "version": "1.10.1",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/50dA9Sha/versions/EkWu7smn/FreshAnimations_v1.10.1.zip",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "YL57xq9U",
+    "name": "Iris Shaders",
+    "description": "A modern shader pack loader for Minecraft intended to be compatible with existing OptiFine shader packs",
+    "version": "1.9.6+1.21.8-fabric",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/YL57xq9U/versions/Rhzf61g1/iris-fabric-1.9.6%2Bmc1.21.8.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "xv94TkTM",
+    "name": "Controlling",
+    "description": "Adds a search bar to the Key-Bindings menu",
+    "version": "26.0.3",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/xv94TkTM/versions/15AHrwi3/Controlling-fabric-1.21.8-26.0.3.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "CVT4pFB2",
+    "name": "Melody",
+    "description": "OpenAL-based library mod for playing background music.",
+    "version": "1.0.12-1.21.6-fabric",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/CVT4pFB2/versions/NsXvHOnn/melody_fabric_1.0.12_MC_1.21.6.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "yBW8D80W",
+    "name": "LambDynamicLights - Dynamic Lights",
+    "description": "Adds dynamic lights to Minecraft as the most feature-complete and optimized dynamic lighting mod.",
+    "version": "4.8.6+1.21.8",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/yBW8D80W/versions/foMsxsVt/lambdynamiclights-4.8.6%2B1.21.8.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "BVzZfTc1",
+    "name": "[ETF] Entity Texture Features",
+    "description": "Emissive, Random & Custom texture support for entities in resourcepacks just like Optifine but for Fabric",
+    "version": "7.0.2",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/BVzZfTc1/versions/ZGrSwKTR/entity_texture_features_1.21.6-fabric-7.0.2.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "NcUtCpym",
+    "name": "Xaero's World Map",
+    "description": "Adds a full screen world map which shows you what you have explored in the world. Works great together with Xaero's Minimap.",
+    "version": "1.39.13_Fabric_1.21.8",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/NcUtCpym/versions/d1Pc1nIN/XaerosWorldMap_1.39.13_Fabric_1.21.8.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "89Wsn8GD",
+    "name": "Capes",
+    "description": "Lets you use capes from OptiFine, LabyMod and other cape mods",
+    "version": "1.5.9+1.21.8",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/89Wsn8GD/versions/GRuX8d2G/capes-1.5.9%2B1.21.8-fabric.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "Wq5SjeWM",
+    "name": "FancyMenu",
+    "description": "Customize Minecraft's menus with ease!",
+    "version": "3.7.1-1.21.8-fabric",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/Wq5SjeWM/versions/vikmNv2I/fancymenu_fabric_3.7.1_MC_1.21.8.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "dspVZXKP",
+    "name": "Fast Better Grass",
+    "description": "Makes grass and related blocks use the top texture on the sides. Works with other resource packs!",
+    "version": "1.21.9",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/dspVZXKP/versions/3bM2pL1r/Fast%20Better%20Grass.zip",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "1bokaNcj",
+    "name": "Xaero's Minimap",
+    "description": "Displays a map of the nearby world terrain, players, mobs, entities in the corner of your screen. Lets you create waypoints which help you find the locations you've marked.",
+    "version": "25.2.16_Fabric_1.21.8",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/1bokaNcj/versions/dEIlpdij/Xaeros_Minimap_25.2.16_Fabric_1.21.8.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "QdG47OkI",
+    "name": "Model Gap Fix",
+    "description": "Fixes gaps in Block Models and Item Models",
+    "version": "1.21.5-1.12",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/QdG47OkI/versions/WcDxGReS/modelfix-1.21.5-1.12-fabric.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "eXts2L7r",
+    "name": "Text Placeholder API",
+    "description": "Placeholder and Text manipulation library for your Minecraft mods.",
+    "version": "2.7.2+1.21.8",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/eXts2L7r/versions/1S1kjZ9W/placeholder-api-2.7.2%2B1.21.8.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "Ha28R6CL",
+    "name": "Fabric Language Kotlin",
+    "description": "This is a mod that enables usage of the Kotlin programming language for Fabric mods.",
+    "version": "1.13.7+kotlin.2.2.21",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/Ha28R6CL/versions/LcgnDDmT/fabric-language-kotlin-1.13.7%2Bkotlin.2.2.21.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "J81TRJWm",
+    "name": "Konkrete",
+    "description": "Just another boring library mod.",
+    "version": "1.9.12-1.21.6-fabric",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/J81TRJWm/versions/ua0GnQ25/konkrete_fabric_1.9.12_MC_1.21.6.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "zV5r3pPn",
+    "name": "3D Skin Layers",
+    "description": "Render the player skin layer in 3d!",
+    "version": "1.9.2",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/zV5r3pPn/versions/4TVWm41c/skinlayers3d-fabric-1.9.2-mc1.21.8.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "AANobbMI",
+    "name": "Sodium",
+    "description": "The fastest and most compatible rendering optimization mod for Minecraft. Now available for both NeoForge and Fabric!",
+    "version": "mc1.21.8-0.7.2-fabric",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/AANobbMI/versions/pJRT8Zxt/sodium-fabric-0.7.2%2Bmc1.21.8.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "ZvMtQlho",
+    "name": "Bliss Shaders",
+    "description": "A well performing fantasy styled shaderpack with emphasis on scene variation and customization.",
+    "version": "2.1.1",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/ZvMtQlho/versions/VObMy7ML/Bliss_v2.1.1_%28Chocapic13_Shaders_edit%29.zip",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "v3CYg2V9",
+    "name": "Drippy Loading Screen",
+    "description": "Addon for FancyMenu to customize the loading screen.",
+    "version": "3.0.15-1.21.6-fabric",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/v3CYg2V9/versions/4OQ0QIuW/drippyloadingscreen_fabric_3.0.15_MC_1.21.6.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "uCdwusMi",
+    "name": "Distant Horizons",
+    "description": "See farther without turning your game into a slide show",
+    "version": "2.3.6-b-1.21.8",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/uCdwusMi/versions/iej5xqn2/DistantHorizons-2.3.6-b-1.21.8-fabric-neoforge.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "NNAgCjsB",
+    "name": "Entity Culling",
+    "description": "Using async path-tracing to hide Block-/Entities that are not visible",
+    "version": "1.9.3",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/NNAgCjsB/versions/U81jswDa/entityculling-fabric-1.9.3-mc1.21.8.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "uXXizFIs",
+    "name": "FerriteCore",
+    "description": "Memory usage optimizations",
+    "version": "8.0.0-fabric",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/uXXizFIs/versions/CtMpt7Jr/ferritecore-8.0.0-fabric.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "P7dR8mSH",
+    "name": "Fabric API",
+    "description": "Lightweight and modular API providing common hooks and intercompatibility measures utilized by mods using the Fabric toolchain.",
+    "version": "0.136.0+1.21.8",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/P7dR8mSH/versions/RMahJx2I/fabric-api-0.136.0%2B1.21.8.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "qyVF9oeo",
+    "name": "Sound Physics Remastered",
+    "description": "A Minecraft mod that provides realistic sound attenuation, reverberation, and absorption through blocks.",
+    "version": "fabric-1.21.8-1.5.1",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/qyVF9oeo/versions/gUaL8lMJ/sound-physics-remastered-fabric-1.21.8-1.5.1.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "fuuu3xnx",
+    "name": "Searchables",
+    "description": "Searchables is a library mod that adds helper methods that allow for searching and filtering elements based on components, as well as offering built in auto-complete functionality.",
+    "version": "1.0.2",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/fuuu3xnx/versions/LLzPBiwT/Searchables-fabric-1.21.8-1.0.2.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "R6NEzAwj",
+    "name": "Complementary Shaders - Unbound",
+    "description": "Transforming the visuals of Minecraft with exceptional quality, detail, and performance.",
+    "version": "r5.6.1",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/R6NEzAwj/versions/LXrX6oqm/ComplementaryUnbound_r5.6.1.zip",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "5ZwdcRci",
+    "name": "ImmediatelyFast",
+    "description": "Speed up immediate mode rendering in Minecraft",
+    "version": "1.12.2+1.21.8-fabric",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/5ZwdcRci/versions/OrO3H19n/ImmediatelyFast-Fabric-1.12.2%2B1.21.8.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "bQhBuv7x",
+    "name": "MCEF [Keksuccino's Fork]",
+    "description": "This is a temporary fork of MCEF by ds58 until they have time to update their fork to support MC 1.21.5+.",
+    "version": "2.1.7-1.21.6-fabric",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/bQhBuv7x/versions/CGtNxcFd/mcef_fabric_2.1.7_MC_1.21.6.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "9eGKb6K1",
+    "name": "Simple Voice Chat",
+    "description": "A working voice chat in Minecraft!",
+    "version": "fabric-1.21.8-2.6.6",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/9eGKb6K1/versions/2Z1g1v36/voicechat-fabric-1.21.8-2.6.6.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "1IjD5062",
+    "name": "Continuity",
+    "description": "A Minecraft mod that allows for efficient connected textures",
+    "version": "3.0.1-beta.1+1.21.6",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/1IjD5062/versions/m0cvWhzT/continuity-3.0.1-beta.1%2B1.21.6.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  },
+  {
+    "id": "mOgUt4GM",
+    "name": "Mod Menu",
+    "description": "Adds a mod menu to view the list of mods you have installed.",
+    "version": "15.0.0",
+    "required": false,
+    "downloadUrl": "https://cdn.modrinth.com/data/mOgUt4GM/versions/am1Siv7F/modmenu-15.0.0.jar",
+    "author": "Modrinth",
+    "inNormalPack": true
+  }
 ];
 
 export const modpacks = [
   {
-    id: "enhanced",
-    name: "Enhanced",
-    version: "4.0.0",
-    downloadUrl: "#",
-    type: "enhanced",
-  },
-  {
-    id: "lite",
-    name: "Lite",
-    version: "4.0.0",
-    downloadUrl: "#",
-    type: "lite",
-  },
+    "id": "enhanced",
+    "name": "Enhanced",
+    "version": "1.0.0",
+    "downloadUrl": "#"
+  }
 ];
 
 export const modpackInfo = {
-  name: "Hak SMP Season 4 Modpack",
-  version: "4.0.0",
-  minecraftVersion: "1.20.1",
-  forgeVersion: "47.2.0",
-  totalMods: plugins.length,
-  lastUpdated: "2024-10-11",
+  "name": "HAK SMP - Landen & Dorpen Editie",
+  "version": "1.0.0",
+  "minecraftVersion": "1.21.8",
+  "forgeVersion": "FABRIC",
+  "totalMods": 34,
+  "lastUpdated": "2025-12-11"
 };
